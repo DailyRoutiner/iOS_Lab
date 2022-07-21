3644ded8cfc9928f4e6338c5330cb04c0edcf0ca
SeongJun <schoi0813@conestogac.on.ca>
2022-07-21 11:26:12 -0400

    first commit

---
+++ Lab6/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lab6
+//
+//  Created by Jun on 2022/07/21.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var tempatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+}
+
